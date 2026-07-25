0c40256605aa487e2d42613b108cb61e57be0546
TV46 <*redacted*>
2026-07-25 12:55:18 +1000

    fresh start on new branch

--- evaluations.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef CHESSENGINE_EVALUATIONS_H
-#define CHESSENGINE_EVALUATIONS_H
-
-#include "boardSetup.h"
-
-int evaluateControl(const map<string, Piece>& simulationBoard);
-
-#endif //CHESSENGINE_EVALUATIONS_H
